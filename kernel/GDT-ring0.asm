e5b776c9fbfa8c0af54ba67aef8ec7e937870610
GRAY_WHALE_CO <memoryflyk@gmail.com>
2026-05-31 12:23:36 +0300

    Update comments in GDT-ring0.asm for clarity

--- kernel/GDT-ring0.asm
+++ kernel/GDT-ring0.asm
@@ -1,16 +1,16 @@
 [bits 32]
 
 gdt_start:
-    ; 1. Null Descriptor (обязательно)
+    ; 1. Null Descriptor (mandatory)
     dd 0x0, 0x0
 
-    ; 2. Code Segment (Селектор 0x08)
-    ; База = 0x0, Лимит = 0xFFFFF, Гранулярность = 4KB, Ring 0, Код
+    ; 2. Code Segment (Selector 0x08)
+    ; Base = 0x0, Limit = 0xFFFFF, Granularity = 4KB, Ring 0, Code
     dw 0xFFFF, 0x0000
     db 0x00, 10011010b, 11001111b, 0x00
 
-    ; 3. Data Segment (Селектор 0x10)
-    ; База = 0x0, Лимит = 0xFFFFF, Гранулярность = 4KB, Ring 0, Данные
+    ; 3. Data Segment (Selector 0x10)
+    ; Base = 0x0, Limit = 0xFFFFF, Granularity = 4KB, Ring 0, Data
     dw 0xFFFF, 0x0000
     db 0x00, 10010010b, 11001111b, 0x00
 gdt_end:
