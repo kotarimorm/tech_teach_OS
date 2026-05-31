b6fd7c069c293ff2aedede73d066d372c7f6a0ed
GRAY_WHALE_CO <memoryflyk@gmail.com>
2026-05-31 12:37:00 +0300

    Update panic.asm

--- kernel/panic.asm
+++ kernel/panic.asm
@@ -2,19 +2,20 @@
 global panic
 
 section .text
-; Вход: ESI = адрес строки с сообщением (нулевой терминатор)
+; Input: ESI = address of the message string (null-terminated)
 panic:
-    cli                 ; Отключаем прерывания, чтобы не мешали
-    mov edi, 0xB8000    ; Адрес VGA-буфера
-    mov ah, 0x4F        ; Атрибуты: красный фон (4), белый текст (F)
+    cli                 ; Disable interrupts to prevent interference
+    cld                 ; Force clear direction flag to ensure forward lodsb/stosw
+    mov edi, 0xB8000    ; VGA frame buffer address
+    mov ah, 0x4F        ; Attributes: red background (4), white text (F)
     
 .loop:
-    lodsb               ; Читаем байт из строки (в AL)
-    cmp al, 0           ; Конец строки?
+    lodsb               ; Load byte from string (into AL)
+    cmp al, 0           ; End of string?
     je .halt
-    stosw               ; Записываем символ + атрибут в видеопамять
+    stosw               ; Write character + attribute to video memory
     jmp .loop
 
 .halt:
-    hlt                 ; Остановка процессора
-    jmp .halt           ; На случай, если процессор проснется
+    hlt                 ; Halt the CPU
+    jmp .halt           ; In case the CPU wakes up
