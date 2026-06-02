684e588aa0e9d69059e887759006e6b2317d3c9e
GRAY_WHALE_CO <memoryflyk@gmail.com>
2026-06-02 15:05:55 +0300

    Refactor init_paging with detailed comments

--- cpu/paging.asm
+++ cpu/paging.asm
@@ -1,28 +1,43 @@
 section .bss
 align 4096
+
 page_directory:
     resb 4096
+
 page_table_0:
     resb 4096
 
+
 section .text
+global init_paging
+
 init_paging:
     pusha
 
-    ; clear page directory
+    ; -----------------------------------
+    ; 1. Clear page directory (1024 entries)
+    ; -----------------------------------
     mov edi, page_directory
     xor eax, eax
     mov ecx, 1024
+    cld
     rep stosd
 
-    ; clear page table 0
+    ; -----------------------------------
+    ; 2. Clear page table (1024 entries)
+    ; -----------------------------------
     mov edi, page_table_0
+    xor eax, eax
     mov ecx, 1024
+    cld
     rep stosd
 
-    ; identity map first 4mb (flags: r/w, present)
+    ; -----------------------------------
+    ; 3. Identity map first 4MB
+    ; -----------------------------------
     mov edi, page_table_0
-    mov eax, 0x00000003
+    mov eax, 0x00000003     ; present + RW
+
     mov ecx, 1024
 .map_pt:
     mov [edi], eax
@@ -30,16 +45,22 @@ init_paging:
     add edi, 4
     loop .map_pt
 
-    ; link page table 0 to directory entry 0
+    ; -----------------------------------
+    ; 4. Link page table into directory
+    ; -----------------------------------
     mov eax, page_table_0
-    or eax, 3
+    or eax, 0x3
     mov [page_directory], eax
 
-    ; load directory base to cr3
+    ; -----------------------------------
+    ; 5. Load CR3
+    ; -----------------------------------
     mov eax, page_directory
     mov cr3, eax
 
-    ; enable paging bit in cr0
+    ; -----------------------------------
+    ; 6. Enable paging (CR0.PG = bit 31)
+    ; -----------------------------------
     mov eax, cr0
     or eax, 0x80000000
     mov cr0, eax
